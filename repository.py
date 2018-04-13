598677dc2353ccc309bd921e737d312c438c6fe9
Forrest Keppler <fkeppler@instructure.com>
2018-04-13 16:17:46 -0700

    about to run full round test

--- repository.py
+++ repository.py
@@ -1,8 +1,18 @@
 import pymongo
 from pymongo import MongoClient
+import json
 
 def get_connection():
     return MongoClient('localhost', 27017)['gitnomic']
+  
+def get_config():
+  with open('config.json') as config_file:
+    conf = json.load(config_file)
+  return conf
+  
+def set_config(new_config):
+  with open('config.json', 'w') as config_file:
+    json.dump(new_config, config_file)
 
 def find_user (username):
     db = get_connection()
@@ -21,6 +31,11 @@ def get_game_state():
   db = get_connection()
   gameDB = db.game
   return gameDB.find_one({"_id" : "gameState"})
+
+def get_current_player():
+  #Gets the player whose turn it is
+  state = get_game_state()
+  return state['currentPlayer']
   
 def advance_round(new_current_player):
   #set a new current player and increment the turn counter
